5d7b58d7ea6709ccf7748b30f4a422b3c702f2f3
romeech91 <romontino@gmail.com>
2018-05-24 13:27:02 +0300

    menu acco fx

--- js/menu-acco.js
+++ js/menu-acco.js
@@ -1,15 +1,25 @@
-var button = document.querySelectorAll('.acco__item__block');
-var cont = document.querySelectorAll('.acco__item__content');
+//acco__item--active
 
-var changeWidth = function(param) {
-    for (var i = 0; i < button.length; i++) {
-        button[i].addEventListener('click', () => {
-            for (var i = 0; i < cont.length; i++) {
-                cont[i].style.width = param;
-            }
-        });
-    }
-}
 
-changeWidth('500px');
+const listItems = document.querySelector('.acco__list');
+const Items = document.querySelectorAll('.acco__item');
+
+listItems.addEventListener('click', (e) => {
+    e.preventDefault();
+    let target = e.target;
+
+    if (target.className === "acco__item__block-txt") {
+        let item = target.closest('.acco__item');
+        if (item.classList.contains('acco__item--active')) {
+            item.classList.toggle('acco__item--active');
+        } else {
+            Items.forEach(function(item, index) {
+                item.classList.remove('acco__item--active');
+            });
+            item.classList.add('acco__item--active');
+        }
+    };
+
+});
+
 
